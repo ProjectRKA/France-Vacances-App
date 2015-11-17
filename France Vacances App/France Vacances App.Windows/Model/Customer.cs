4dea070e75ac890b2ca3f45cb51d3479ea52b528
Rene Keilow Albrechtsen <r_albrechtsen@outlook.com>
2015-11-17 12:09:12 +0100

    Added customer and customerlist class

---
+++ France Vacances App/France Vacances App.Windows/Model/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace App2.Model
+{
+    //constructor
+    class Customer
+    {
+        private string _password;
+        private string _username;
+
+
+
+        public Customer(string _password, string _username)
+        {
+            this._password = _password;
+            this._username = _username;
+        }
+
+        public void Login()
+        {
+
+        }
+
+
+
+
+    }
+}
---
+++ France Vacances App/France Vacances App.Windows/Model/CustomerList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2.Model
+{
+    class CustomerList
+    {
+        private string x; //username and password connected by magic to textboxes
+        private string y;
+
+        List<Customer> list;
+
+        public CustomerList()
+        {
+            list = new List<Customer>();
+        }
+
+        public void Register()
+        {
+            list.Add(new Customer(x, y));
+        }
+
+
+    }
+}
